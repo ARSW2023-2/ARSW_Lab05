dce818f9a942cfa9103d6851c13ebd9ae00e3adf
20042000 <karol.ladino@mail.escuelaing.edu.co>
2023-09-22 17:01:49 -0500

    Parte I puntos 3 y 4

--- src/main/java/edu/eci/arsw/blueprints/controllers/BlueprintAPIController.java
+++ src/main/java/edu/eci/arsw/blueprints/controllers/BlueprintAPIController.java
@@ -7,18 +7,43 @@ package edu.eci.arsw.blueprints.controllers;
 
 import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+
+import edu.eci.arsw.blueprints.services.BlueprintsServices;
 
 /**
  *
  * @author hcadavid
  */
+@RestController
+@RequestMapping(value = "/blueprints")
 public class BlueprintAPIController {
+
+    @Autowired
+    @Qualifier("Blueprint")
+    private BlueprintsServices bps;
     
-    
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<?> getAllBlue(){
+        try{
+            //obtener datos que se enviarán a través del API
+            return new ResponseEntity<>(bps.getAllBlueprints(),HttpStatus.ACCEPTED);
+
+        }catch(Exception e){
+            Logger.getLogger(BlueprintAPIController.class.getName()).log(Level.SEVERE, null, e);
+            return new ResponseEntity<>("Error al traer los Blueprint",HttpStatus.NOT_FOUND);
+        }
+    }
     
     
     

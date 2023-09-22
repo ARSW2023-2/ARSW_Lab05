034ef701951d7c9f7a022e21d0be20dfb6f38aaf
20042000 <karol.ladino@mail.escuelaing.edu.co>
2023-09-22 16:28:40 -0500

    Parte I punto 2

--- src/main/java/edu/eci/arsw/blueprints/persistence/impl/InMemoryBlueprintPersistence.java
+++ src/main/java/edu/eci/arsw/blueprints/persistence/impl/InMemoryBlueprintPersistence.java
@@ -34,7 +34,21 @@ public class InMemoryBlueprintPersistence implements BlueprintsPersistence{
         Point[] pts=new Point[]{new Point(140, 140),new Point(115, 115)};
         Blueprint bp=new Blueprint("_authorname_", "_bpname_ ",pts);
         blueprints.put(new Tuple<>(bp.getAuthor(),bp.getName()), bp);
-        
+
+        Blueprint bp1=new Blueprint("Luisa", "Plano1",pts);
+        Blueprint bp2=new Blueprint("Luisa", "Plano2",pts);
+
+        Blueprint bp3=new Blueprint("karol", "Plano3",pts);
+        Blueprint bp4=new Blueprint("Fernanda", "Plano4",pts);
+        Blueprint bp5=new Blueprint("Daniela", "Plano5",pts);
+
+        blueprints.put(new Tuple<>(bp.getAuthor(),bp.getName()), bp);
+        blueprints.put(new Tuple<>(bp1.getAuthor(),bp1.getName()), bp1);
+        blueprints.put(new Tuple<>(bp2.getAuthor(),bp2.getName()), bp2);
+
+        blueprints.put(new Tuple<>(bp3.getAuthor(),bp3.getName()), bp3);
+        blueprints.put(new Tuple<>(bp4.getAuthor(),bp4.getName()), bp4);
+        blueprints.put(new Tuple<>(bp5.getAuthor(),bp5.getName()), bp5);
     }    
     
     @Override
